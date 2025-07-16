f2691700363aae13c2dbe6e8e892c1ca4cbe5877
Dima Ivashko <28mail@mail.ru>
2025-07-16 23:31:00 +0300

    glove80

--- config/default.nix
+++ config/default.nix
@@ -1,11 +1,23 @@
-{ pkgs ?  import <nixpkgs> {}
-, firmware ? import ../src {}
+{ pkgs ? import <nixpkgs> {}
+, firmware ? import ../src/default.nix { inherit pkgs; }  # => імпортуємо ZMK build функції
 }:
 
 let
   config = ./.;
 
-  glove80_left  = firmware.zmk.override { board = "glove80_lh"; keymap = "${config}/glove80.keymap"; kconfig = "${config}/glove80.conf"; };
-  glove80_right = firmware.zmk.override { board = "glove80_rh"; keymap = "${config}/glove80.keymap"; kconfig = "${config}/glove80.conf"; };
+  glove80_left = firmware.zmk.override {
+    board  = "glove80_lh";
+    keymap = "${config}/glove80.keymap";
+    kconfig = "${config}/glove80.conf";
+  };
 
-in firmware.combine_uf2 glove80_left glove80_right "glove80"
+  glove80_right = firmware.zmk.override {
+    board  = "glove80_rh";
+    keymap = "${config}/glove80.keymap";
+    kconfig = "${config}/glove80.conf";
+  };
+in
+
+# Деякі версії combine_uf2 приймають (left right name), інші — лише (left right).
+# Якщо твій build знову впаде, спробуй варіант без name.
+firmware.combine_uf2 glove80_left glove80_right "glove80"
